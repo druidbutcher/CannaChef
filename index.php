aa73790651f19a50060bca4003a8bbf24dde7f84
druidbutcher <157072357+druidbutcher@users.noreply.github.com>
2024-10-17 07:24:17 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,257 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="css/style.css" />
-    
-    <title>Dev'Back office tools PWA</title>
-	<link rel="manifest" href="manifest.json" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
 
-	
-  </head>
-  <body>
- <?php
-     session_unset();
-     session_destroy();
-     if (isset($_SESSION['demouser'])) {
-        //echo ("demouser is set");
-     }else{
-      //echo ("No demouser");
-     }
-     ?>
-  <div class="navbar">
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-    <a href="help.php">Help</a>
-  
-    </div>
-  </div>
-</div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 
+    <link rel="stylesheet" href="css/stylemain.css">
+</head>
+
+<body>
+
+    <!-- navbar section   -->
+
+    <header class="navbar-section">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><i class="bi bi-chat"></i> Canna Chef</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="demo-login.php">Demo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#projects">News and info</a>
+                        </li>
  
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">login</a>
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-  
-  <a href="register.php">Register</a><br>
-  <a href="Login.php">Login</a><br>
-  <a href="demo-login.php">Run in Demo Mode</a><br>
+    <!-- hero section  -->
 
-  </body>
+    <section id="home" class="hero-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-sm-12 text-content">
+                    <h1>Make edibles with precise dosing</h1>
+                    <p>We take the guesswork out of making edibles.
+                    </p>
+
+                 
+                    <script async
+                    <script async
+                    <script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+<a href="https://buy.stripe.com/test_7sI6oqgyU1spd445ko" onclick="close_window();">close</a>
+<stripe-buy-button
+
+  buy-button-id="buy_btn_1Q8ivbCao4lQnH3VOLgK7Bib"
+  publishable-key="pk_test_51JSU5hCao4lQnH3V7VPn8SMHrp2awVTQXnR0icb2KM4NwMcSCvy4JdiPcrjxE6G3Kn2z4iY9FVJnGiyFwkIb8Aj000gxoikWjf"
+>
+</stripe-buy-button>
+
+
+                </div>
+                <div class="col-lg-8 col-md-12 col-sm-12">
+                    <img src="images/ccicon1024.png" alt="" class="img-fluid">
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- project section  -->
+
+    <section class="project-section" id="projects">
+        <div class="container">
+            <div class="row text">
+                <div class="col-lg-6 col-md-12">
+                    <h3>Some yadayada</h3>
+                    <h1>Stuff to see</h1>
+                    <hr>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <p>Put things here.</p>
+                </div>
+            </div>
+            <div class="row project">
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img src="images/cannaleaf.png" width="100" height="200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="text">
+                                <h4 class="card-title">Teaser1</h4>
+                                <p class="card-text">Some small info</p>
+                                <button>see project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img src="images/buttercream.jpg" width="100" height="200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="text">
+                                <h4 class="card-title">Teaser 2</h4>
+                                <p class="card-text">maybe wellness info</p>
+                                <button>see project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img src="images/margaritas.jpg" width="100" height="200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="text">
+                                <h4 class="card-title">Teaser3</h4>
+                                <p class="card-text">Maybe walkthrough of demo</p>
+                                <button>see project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img src="images/gummies1.jpeg" width="100" height="200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="text">
+                                <h4 class="card-title">teaser4</h4>
+                                <p class="card-text">Why to buy full version</p>
+                                <button>see project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- contact section  -->
+
+    <section class="contact-section" id="contact">
+        <div class="container">
+
+            <div class="row gy-4">
+
+                <h1>contact us</h1>
+                <div class="col-lg-6">
+
+                    <div class="row gy-4">
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-geo-alt"></i>
+                                <h3>Address</h3>
+                                <p>A108 taco Street,<br>Mazatlan,SN. 535022</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-telephone"></i>
+                                <h3>Call Us</h3>
+                                <p>+91 9876545672<br>+91 8763456243</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-envelope"></i>
+                                <h3>Email Us</h3>
+                                <p>feedback@druidsforge.com<br>service@druidsforge.com</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box">
+                            <i class="bi bi-clock"></i>
+                                <h3>Social Media</h3>
+                                <p>facebook<br>instagram </p>
+                            </div>
+                        </div>
+                       
+                    </div>
+
+                </div>
+
+               
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- footer section  -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-12 col-sm-12">
+                    <p class="logo"><i class="bi bi-chat"></i> Druids Forge</p>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <ul class="d-flex">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Demo</a></li>
+                        <li><a href="#">Info</a></li>
+                        <li><a href="#">contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-12 col-sm-12">
+                    <p>&copy;2024_Druids Forge</p>
+                </div>
+
+                <div class="col-lg-1 col-md-12 col-sm-12">
+                    <!-- back to top  -->
+
+                    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+                            class="bi bi-arrow-up-short"></i></a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
